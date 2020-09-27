0e04e7342c6046c5b96354bc4409c686b6452e10
Sakthyvell <sakthyvellsuperamaniam@gmail.com>
2020-09-27 22:10:21 +0800

    Some HTML Templates

--- app.py
+++ app.py
@@ -8,15 +8,7 @@ app = Flask(__name__)
 @app.route('/index')
 @app.route('/home')
 def main():
-    return render_template('index.html')
-
-
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
