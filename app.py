eee7203aa11a20e8c8d60d9f283c0a49b88dbf1e
Sakthyvell <sakthyvellsuperamaniam@gmail.com>
2021-03-02 22:10:37 +0800

    Editing app.py

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-import os
-
 from flask import Flask, render_template
 
-
-def create_app(test_config=None):
-    
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-
-    @app.route('/')
-    def home():
-        return render_template("index.html");
-
-    return app
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template("index.html");
 
 
 if __name__ == '__main__':
-    create_app()
+    app.run()
